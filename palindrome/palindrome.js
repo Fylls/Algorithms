4aed4aeb0a7417e1de8d3e355e43afc0663c182e
Fylls <sergentifilippo@gmail.com>
2020-05-07 17:10:06 +0200

    new algos
    new algos

---
+++ palindrome/palindrome.js
@@ -0,0 +1,25 @@
+// Given a string, f() returns whether the string is a palindrome (true) or not (false)
+// Palindromes are words that read the same backwards and forwards.
+// f() it is case insensitive!
+
+// palindrome("Madam") === true
+// palindrome("love") === false
+
+// fast implementation
+function palindrome(str) {
+  str = str.toLowerCase();
+  return str === str.split("").reverse().join("");
+}
+
+function palindrome2(str) {
+  str = str.toLowerCase();
+  // takes str, puts every character inside and array, swaps every index of the array
+  // and joins every item of the reversed array
+  const reversed = str.split("").reverse().join("");
+
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
