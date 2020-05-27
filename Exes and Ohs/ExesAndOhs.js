b971a3f47967c5aac54bf4029397bda3b637eb0e
Fylls <sergentifilippo@gmail.com>
2020-05-27 11:52:54 +0200

    ex

--- Exes and Ohs/ExesAndOhs.js
+++ Exes and Ohs/ExesAndOhs.js
@@ -9,19 +9,20 @@
 // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 // XO("zzoo") => false
 
+// explained
 function XO(str) {
   let array = str.toLowerCase().split("");
 
-  let numberX;
-  let numberO;
+  let numberX = 0;
+  let numberO = 0;
 
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "x") {
-      numberX = +1;
+      numberX += 1;
     }
 
     if (array[i] === "o") {
-      numberO = +1;
+      numberO += 1;
     }
   }
 
@@ -32,4 +33,33 @@ function XO(str) {
   } else {
     return false;
   }
+}
+
+// regex
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
+
+// replace
+function XO(str) {
+  var a = str.replace(/x/gi, ""),
+    b = str.replace(/o/gi, "");
+  return a.length === b.length;
+}
+
+// filter
+const XO = (str) => {
+  str = str.toLowerCase().split("");
+  return (
+    str.filter((x) => x === "x").length === str.filter((x) => x === "o").length
+  );
+};
+
+// split
+function XO(str) {
+  return (
+    str.toLowerCase().split("x").length === str.toLowerCase().split("o").length
+  );
 }
