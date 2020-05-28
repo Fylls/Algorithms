c89039f05a15b01f31302b3375165fc60b476aeb
Fylls <sergentifilippo@gmail.com>
2020-05-28 14:39:21 +0200

    .

--- Drawing Algorithms/BuildTower/buildTower.js
+++ Drawing Algorithms/BuildTower/buildTower.js
@@ -1,8 +1,8 @@
-//Build Tower
-//Build Tower by the following given argument:
-//number of floors (integer and always greater than 0).
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
 
-//Tower block is represented as *
+// Tower block is represented as *
 
 /*
 for example, a tower of 3 floors looks like below
--- Drawing Algorithms/pyramid/pyramid.js
+++ Drawing Algorithms/pyramid/pyramid.js
@@ -18,7 +18,7 @@ pyramid(3)
 function pyramid(n) {
   for (let i = 1; i <= n; i++) {
     const padding = " ".repeat(n - i);
-    const hashes = "#".repeat(i * 2 - 1);
+    const hashes = "*".repeat(i * 2 - 1);
     console.log(padding + hashes + padding);
   }
 }
@@ -27,7 +27,7 @@ function pyramid1(n) {
   for (let i = 0; i <= n - 1; i++) {
     let step = "";
     step += " ".repeat(n - i - 1);
-    step += "#".repeat(2 * i + 1);
+    step += "*".repeat(2 * i + 1);
     step += " ".repeat(n - i - 1);
     console.log(step);
   }
