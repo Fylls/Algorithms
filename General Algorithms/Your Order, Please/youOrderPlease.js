f27c607ee8082d5b0e174561430151a422d1aab9
Fylls <sergentifilippo@gmail.com>
2020-06-13 09:28:03 +0200

    order

---
+++ General Algorithms/Your Order, Please/youOrderPlease.js
@@ -0,0 +1,44 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  const array = words.split(" ");
+  const res = [];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i].includes(j + 1)) {
+        res[j] = array[i];
+      }
+    }
+  }
+
+  return res.join(" ");
+}
+
+// regex
+var reg = /\d/;
+
+function order(words) {
+  return words.split(" ").sort(comparator).join(" ");
+}
+
+function comparator(word, nextWord) {
+  return +word.match(reg) - +nextWord.match(reg);
+}
+
+// onliner
+// const order = w => w.split(' ').sort((a, b) => /\d/.exec(a) - /\d/.exec(b)).join(' ');
+const order = (w) =>
+  w
+    .split(" ")
+    .sort((a, b) => /\d/.exec(a) - /\d/.exec(b))
+    .join(" ");
