28938174955bb43b18dbe7b3ea4b28d8d4a6b3cd
Fylls <sergentifilippo@gmail.com>
2020-06-13 18:58:15 +0200

    asfasdfasd

---
+++ Contextualized Algorithms/Bit Counting/bitCounting.js
@@ -0,0 +1,28 @@
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+// You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+const countBits = function (n) {
+  const binary = n.toString(2);
+  const array = binary.split("");
+
+  let numerOfOnes = 0;
+
+  array.forEach((el) => {
+    if (el === "1") numerOfOnes++;
+  });
+
+  return numerOfOnes;
+};
+
+countBits = (n) => n.toString(2).split("0").join("").length;
+
+function countBits(n) {
+  for (c = 0; n; n >>= 1) c += n & 1;
+  return c;
+}
+
+var countBits = function (n) {
+  return n.toString(2).replace(/0/g, "").length;
+};
